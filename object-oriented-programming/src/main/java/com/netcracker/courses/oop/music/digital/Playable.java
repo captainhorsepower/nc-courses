f21e65c693d273861a3dd4960f0816a9986ecc16
Artem Vorobej <captainhorsepower228@gmail.com>
2019-03-20 04:22:04 +0300

    docs(Playable): express my intention in docs

--- object-oriented-programming/src/main/java/com/netcracker/courses/oop/music/digital/Playable.java
+++ object-oriented-programming/src/main/java/com/netcracker/courses/oop/music/digital/Playable.java
@@ -3,6 +3,7 @@ package com.netcracker.courses.oop.music.digital;
 /**
  * General interface for stuff that can be played
  * e.g. song, album...
+ * I will implement this on songs and CDs later today, if I have time util deadline
  */
 public interface Playable {
     void play();
