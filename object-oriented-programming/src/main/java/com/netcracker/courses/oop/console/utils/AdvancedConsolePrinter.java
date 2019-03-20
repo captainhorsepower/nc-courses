d13b0c61600c2a8f01dea356dbe08162ca8082e8
Artem Vorobej <captainhorsepower228@gmail.com>
2019-03-20 12:25:17 +0300

    style(AdvancedConsolePrinter): align assignments

--- object-oriented-programming/src/main/java/com/netcracker/courses/oop/console/utils/AdvancedConsolePrinter.java
+++ object-oriented-programming/src/main/java/com/netcracker/courses/oop/console/utils/AdvancedConsolePrinter.java
@@ -2,11 +2,11 @@ package com.netcracker.courses.oop.console.utils;
 
 public interface AdvancedConsolePrinter extends ConsolePrinter {
 
-    String DEFAULT_CHOOSE_ITEM_MESSAGE = "choose ";
-    String DEFAULT_INVALID_ITEM_MESSAGE = "\tplease, enter valid ";
-    String DEFAULT_CONSOLE_FAILED_MESSAGE = "console input error. Aborting current task...";
-    String DEFAULT_SELECT_FROM_LIST_MESSAGE = "select one of the following:";
-    String DEFAULT_SELECTION_LIST_IS_EMPTY_MESSAGE = "there are no items to select from!";
+    String DEFAULT_CHOOSE_ITEM_MESSAGE              = "choose ";
+    String DEFAULT_INVALID_ITEM_MESSAGE             = "\tplease, enter valid ";
+    String DEFAULT_CONSOLE_FAILED_MESSAGE           = "console input error. Aborting current task...";
+    String DEFAULT_SELECT_FROM_LIST_MESSAGE         = "select one of the following:";
+    String DEFAULT_SELECTION_LIST_IS_EMPTY_MESSAGE  = "there are no items to select from!";
 
     @Override
     default void printMessage(String message) {
