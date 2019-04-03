0b931ddd6c5103ac13999145b8853e25eff64a30
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-04 02:00:37 +0300

    feat: update Item and Order entities for DAO

--- src/main/java/by/courses/vorobey/artem/entity/Item.java
+++ src/main/java/by/courses/vorobey/artem/entity/Item.java
@@ -1,19 +1,32 @@
 package by.courses.vorobey.artem.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor(staticName = "of")
+@RequiredArgsConstructor(staticName = "of")
 public class Item {
-    @Getter
-    @Setter
-    private int price;
 
+    /** item id (primary key int items table) */
     @Getter
     @Setter
+    private int itemId;
+
+    /** item name */
+    @Getter
+    @Setter
+    @NonNull
     private String name;
 
+    /** item price */
     @Getter
-    private final int itemId;
+    @Setter
+    @NonNull
+    private float price;
+
+    @Override
+    public String toString() {
+        return "Item : id=" + itemId
+                + ", name=\'" + name.trim() + "\'"
+                + ", price=" + price + ";";
+    }
 }
