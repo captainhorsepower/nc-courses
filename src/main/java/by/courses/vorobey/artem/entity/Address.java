b97b16266f4f787d115b07d02a8ee57de5bd27df
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-03 23:05:58 +0300

    docs: add doc to Address

--- src/main/java/by/courses/vorobey/artem/entity/Address.java
+++ src/main/java/by/courses/vorobey/artem/entity/Address.java
@@ -1,29 +1,42 @@
 package by.courses.vorobey.artem.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@AllArgsConstructor(staticName = "of")
+@RequiredArgsConstructor(staticName = "of")
 public class Address {
 
+    /*
+     * address_id in customers table.
+     * Setter should only be called from CustomerDao,
+     * when new customer is added to db ( to set his resulting id )
+     */
     @Getter
     @Setter
     private int addressId;
 
+    /**
+     * customer, which uses this address.
+     * Is set after customer is added to db;
+     * Is represented by customer_id in addresses table;
+     * Used for one to one mapping;
+     */
     @Getter
     @Setter
     private Customer customer;
 
+    /* some addr fields... */
     @Getter
     @Setter
+    @NonNull // lombok annotation for @RequiredArgsConstructor
     private String city;
 
     @Getter
     @Setter
+    @NonNull
     private String street;
 
     @Getter
     @Setter
+    @NonNull
     private int buildNumber;
 }
