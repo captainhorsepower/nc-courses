95fdd321056c862dedcf40fe75dd560fa65bec5c
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-04 00:42:21 +0300

    docs: add doc to Customer

--- src/main/java/by/courses/vorobey/artem/entity/Customer.java
+++ src/main/java/by/courses/vorobey/artem/entity/Customer.java
@@ -7,20 +7,31 @@ import java.sql.Date;
 @RequiredArgsConstructor(staticName = "of")
 public class Customer {
 
+    /*
+     * customer_id in customers table.
+     * Setter should only be called from CustomerDao,
+     * when new customer is added to db ( to set his resulting id )
+     */
     @Getter
     @Setter
     private int customerId;
 
+    /** unique username in db */
     @Getter
     @Setter
     @NonNull
     private String nickname;
 
+    /** customer birthday date */
     @Getter
     @Setter
     @NonNull
     private Date birthday;
 
+    /**
+     * address where this customer's orders should be delivered.
+     * represents one to one mapping (one customer to one address)
+     */
     @Getter
     @NonNull
     private final Address address;
