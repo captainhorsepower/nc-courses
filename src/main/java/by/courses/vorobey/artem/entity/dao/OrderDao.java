a7060588a2b0abadb928d62b8ef99501250bb8c2
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-04 16:12:21 +0300

    feat: implement OrderDao

--- src/main/java/by/courses/vorobey/artem/entity/dao/OrderDao.java
+++ src/main/java/by/courses/vorobey/artem/entity/dao/OrderDao.java
@@ -1,4 +1,396 @@
 package by.courses.vorobey.artem.entity.dao;
 
-public class OrderDao {
+import by.courses.vorobey.artem.entity.Item;
+import by.courses.vorobey.artem.entity.Order;
+import by.courses.vorobey.artem.utils.DatabaseManager;
+import by.courses.vorobey.artem.utils.PostgreSQLDatabaseManager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderDao implements DAO<Order> {
+
+    private static final String ORDERS_TABLE_NAME = "orders";
+    private static final String ORDER_INSERT_PARAM_LIST =
+            "(customer_id, item_count, total_price, order_date)";
+
+    private static final String ORDER_ITEMS_TABLE_NAME = "order_items_pending";
+    private static final String ORDER_ITEMS_INSERT_PARAM_LIST = "(order_id, item_id)";
+
+    private DatabaseManager manager =
+            PostgreSQLDatabaseManager.getManager();
+
+    private void createOrderItemRecord(Long orderId, Long itemId) throws SQLException {
+
+        if (orderId == null || itemId == null) {
+            System.out.println("something wrong with given order and item id, cannot add this record...");
+            return;
+        }
+
+        String insertItemSQL = "INSERT INTO " + ORDER_ITEMS_TABLE_NAME
+                + ORDER_ITEMS_INSERT_PARAM_LIST + "VALUES(?, ?)";
+
+        Connection c = manager.getConnection();
+        PreparedStatement st = c.prepareStatement(insertItemSQL, Statement.RETURN_GENERATED_KEYS);
+
+        st.setInt(1, Math.toIntExact(orderId));
+        st.setInt(2, Math.toIntExact(itemId));
+
+        st.executeUpdate();
+
+        ResultSet rs = st.getGeneratedKeys();
+
+        if (rs.next()) {
+            System.out.println("added order-item record" + " to " + ORDER_ITEMS_TABLE_NAME + " table");
+        }
+
+        st.close();
+
+        manager.closeConnection(c);
+
+    }
+
+    private void updateOrderItemRecord(Long recordId, Long itemId) throws SQLException {
+        String updateRecordSQL =
+                "UPDATE " + ORDER_ITEMS_TABLE_NAME
+                        + " SET"
+                        + " item_id = " + itemId
+                        + " WHERE record_id = " + recordId;
+
+        Connection c = manager.getConnection();
+        PreparedStatement st = c.prepareStatement(updateRecordSQL);
+
+        st.executeUpdate();
+
+        System.out.println("updated order-item record");
+
+        st.close();
+
+        manager.closeConnection(c);
+    }
+
+    private void deleteOrderItemRecord(Long recordId) throws SQLException {
+        String deleteRecordSQL = "DELETE FROM " + ORDER_ITEMS_TABLE_NAME
+                + " WHERE record_id = " + recordId;
+
+        Connection c = manager.getConnection();
+        PreparedStatement st = c.prepareStatement(deleteRecordSQL);
+
+        st.executeUpdate();
+
+        System.out.println("deleted order-item record");
+
+        st.close();
+
+        manager.closeConnection(c);
+    }
+
+    private void updateOrderItemsRecords(Order order) throws SQLException {
+
+        String findOldItemsSQL = "SELECT record_id, item_id FROM " + ORDER_ITEMS_TABLE_NAME
+                + " WHERE " + ORDER_ITEMS_TABLE_NAME + ".order_id = " + order.getOrderId();
+
+        Connection c = manager.getConnection();
+        PreparedStatement st = c.prepareStatement(findOldItemsSQL);
+
+        ResultSet rs = st.executeQuery();
+
+
+        /*
+         * this list contains record_ids of order-item records,
+         * that should no longer be included in given order.
+         */
+        List<Integer> oldExtraItemsRecordIDs = new ArrayList<>(rs.getFetchSize());
+        /*
+         * this is list of item_ids in updated order items
+         */
+        List<Integer> newItemIDs = order.getItems().stream()
+                .map(Item::getItemId)
+                .collect(Collectors.toList());
+
+        /*
+         * after this while loop
+         * 1)  oldExtraItemsRecordIDs will contain all record_ids,
+         *      that should be overwritten or updated.
+         * 2)  newItemIDs will contain only items, that are in
+         *      updated order, but aren't in the old order.
+         */
+        while (rs.next()) {
+
+            Integer oldItemID = rs.getInt("item_id");
+
+            /*
+             * if new items contain item already stored item ->
+             * then there is no need to update it, should keep it.
+             * otherwise, this is extra item
+             */
+            if (!newItemIDs.remove(oldItemID)) {
+                Integer extraRecordId = rs.getInt("record_id");
+                oldExtraItemsRecordIDs.add( extraRecordId );
+            }
+        }
+
+        while (!newItemIDs.isEmpty()) {
+            if (oldExtraItemsRecordIDs.isEmpty()) {
+                createOrderItemRecord(
+                        (long) order.getOrderId(), (long) newItemIDs.remove(0)
+                );
+            } else {
+                updateOrderItemRecord(
+                        (long) oldExtraItemsRecordIDs.remove(0), (long) newItemIDs.remove(0));
+            }
+        }
+
+        while (!oldExtraItemsRecordIDs.isEmpty()) {
+            deleteOrderItemRecord( (long) oldExtraItemsRecordIDs.remove(0) );
+        }
+
+        st.close();
+
+        manager.closeConnection(c);
+    }
+
+    @Override
+    public Order create(Order order) {
+
+        if (order == null) {
+            System.out.println("cannot add null-order!");
+            return null;
+        }
+
+        try {
+
+            String insertOrderSQL = "INSERT INTO " + ORDERS_TABLE_NAME + ORDER_INSERT_PARAM_LIST
+                        + "VALUES(?, ?, ?, ?)";
+
+            Connection c = manager.getConnection();
+            PreparedStatement st = c.prepareStatement(insertOrderSQL, Statement.RETURN_GENERATED_KEYS);
+
+            st.setInt(1, order.getCustomerId());
+            st.setInt(2, order.getItemCount());
+            st.setFloat(3, order.getPrice());
+            st.setDate(4, order.getOrderDate());
+
+            st.executeUpdate();
+
+            ResultSet rs = st.getGeneratedKeys();
+
+            /*
+             * in case order was added successfully
+             * I update customer id inside in customer object
+             * and customer inside address object
+             */
+            if (rs.next()) {
+                int generatedOrderID = rs.getInt("order_id");
+
+                order.setOrderId(generatedOrderID);
+
+                System.out.println("added " + order + " to " + ORDERS_TABLE_NAME + " table");
+
+//                order.getItems().stream()
+//                        .map( i -> (long) i.getItemId() )
+//                        .forEach( itemId -> {
+//                            try {
+//                                createOrderItemRecord((long) order.getOrderId(), itemId);
+//                            } catch (SQLException e) {
+//                                e.printStackTrace();
+//                            }
+//                        });
+
+                /* wanted to use stream, but there is a problem with catching exception */
+                for (Item i : order.getItems()) {
+                    createOrderItemRecord((long) order.getOrderId(),(long) i.getItemId());
+                }
+
+            } else {
+                System.out.println("order wasn't added");
+            }
+
+            st.close();
+
+            manager.closeConnection(c);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return order;
+    }
+
+    @Override
+    public Order read(Long id) {
+
+        if (id == null) {
+            System.out.println("cannot find null-id order!");
+            return null;
+        }
+
+        Order order = null;
+
+        try {
+
+            String findOrderSQL = "SELECT * FROM "
+                    + ORDERS_TABLE_NAME
+                    + " JOIN " + ORDER_ITEMS_TABLE_NAME
+                    + " ON orders.order_id = order_items_pending.order_id"
+                    + " WHERE orders.order_id = " + id;
+
+            Connection c = manager.getConnection();
+            PreparedStatement st = c.prepareStatement(findOrderSQL);
+
+            ResultSet rs = st.executeQuery();
+
+            /* rs should contain <= 1 result */
+            if (rs.next()) {
+
+                order = Order.of();
+                order.setOrderDate(rs.getDate("order_date"));
+                order.setCustomerId(rs.getInt("customer_id"));
+                order.setOrderId(rs.getInt("order_id"));
+
+                int expectedItemCount = rs.getInt("item_count");
+
+                List<Item> items = new ArrayList<>(expectedItemCount);
+
+                ItemDao itemDao = new ItemDao();
+
+                do {
+                    Item item = itemDao.read((long) rs.getInt("item_id"));
+                    items.add(item);
+                    expectedItemCount--;
+                }
+                while(rs.next());
+
+                order.setItems(items);
+
+                System.out.println("read : " + order);
+                System.out.println("items delta = " + expectedItemCount);
+            }
+
+            st.close();
+
+            manager.closeConnection(c);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return order;
+    }
+
+    /**
+     * updated allowed order properties and corresponding db records.
+     * You are allowed to update
+     *  'order date'
+     *   items in list (remove are add new from available items)
+     * @param order order with updates
+     * @return updated order
+     */
+    @Override
+    public Order update(Order order) {
+
+        if (order == null) {
+            System.out.println("cannot update null order!");
+            return null;
+        }
+
+        try {
+
+
+            String updateOrderSQL =
+                    "UPDATE " + ORDERS_TABLE_NAME
+                            + " SET"
+                            + " order_date = ?,"
+                            + " item_count = ?,"
+                            + " total_price = ?"
+                            + " WHERE order_id = " + order.getOrderId();
+
+            Connection c = manager.getConnection();
+            PreparedStatement st = c.prepareStatement(updateOrderSQL);
+
+            st.setDate(1, order.getOrderDate());
+            st.setInt(2, order.getItemCount());
+            st.setFloat(3, order.getPrice());
+
+            int updatedRowCount = st.executeUpdate();
+
+            updateOrderItemsRecords(order);
+
+            System.out.println("updated " + updatedRowCount + " order(s)");
+
+            st.close();
+
+            manager.closeConnection(c);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return order;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+        if (id == null) {
+            System.out.println("cannot delete null-id order!");
+            return;
+        }
+
+        try {
+
+            String deleteOrderSQL =
+                    "DELETE FROM " + ORDERS_TABLE_NAME
+                            + " WHERE order_id = " + id + ";\n"
+                    + "DELETE FROM " + ORDER_ITEMS_TABLE_NAME
+                            + " WHERE order_id = " + id;
+
+            Connection c = manager.getConnection();
+            PreparedStatement st = c.prepareStatement(deleteOrderSQL);
+
+            int deletedRowCount = st.executeUpdate();
+
+            System.out.println("deleted " + deletedRowCount + " order(s)");
+
+            st.close();
+
+            manager.closeConnection(c);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Order> readAll() {
+        List<Order> orders = new ArrayList<>();
+
+        try {
+
+            String readAllOrderIDsSQL = "SELECT order_id FROM " + ORDERS_TABLE_NAME;
+
+            Connection c = manager.getConnection();
+            PreparedStatement st = c.prepareStatement(readAllOrderIDsSQL);
+
+            ResultSet rs = st.executeQuery();
+
+            while(rs.next()) {
+                orders.add( read( (long) rs.getInt("order_id")) );
+            }
+
+            st.close();
+
+            manager.closeConnection(c);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return orders;
+    }
 }
