b9c892663400354d83f75279ccfcb1abd17db6f6
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-09 19:29:18 +0300

    feat: create multi module maven project

---
+++ customer-management/src/main/java/com/netcracker/edu/varabey/CustomerManagementApplication.java
@@ -0,0 +1,7 @@
+package com.netcracker.edu.varabey;
+
+public class CustomerManagementApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ processor/src/main/java/com/netcracker/edu/varabey/ProcessorApplication.java
@@ -0,0 +1,7 @@
+package com.netcracker.edu.varabey;
+
+public class ProcessorApplication {
+    public static void main(String[] args) {
+
+    }
+}
