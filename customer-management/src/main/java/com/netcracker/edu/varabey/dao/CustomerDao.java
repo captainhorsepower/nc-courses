87661ff37c29c30464b13b8ff57ff51cea241030
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-10 20:28:06 +0300

    feat: add CustomerDao

---
+++ customer-management/src/main/java/com/netcracker/edu/varabey/dao/CustomerDao.java
@@ -0,0 +1,13 @@
+package com.netcracker.edu.varabey.dao;
+
+import com.netcracker.edu.varabey.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDao {
+    Customer create(Customer customer);
+    Customer read(Long id);
+    Customer update(Customer customer);
+    void delete(Customer customer);
+    List<Customer> readAll();
+}
