4e4abf884821055e8cba31cec14ac089dcca2a28
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-14 22:22:39 +0300

    feat: finish with Offer entity

--- catalog/src/main/java/com/netcracker/edu/varabey/entity/Offer.java
+++ catalog/src/main/java/com/netcracker/edu/varabey/entity/Offer.java
@@ -8,6 +8,16 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.StringJoiner;
 
+/**
+ * offer entity class.
+ * Stores information about name, tags, category and price.
+ *
+ * Any field, that you can get by getter, of an initialized
+ * (created or read via service) offer, is valid. Offers, stored in
+ * category are not loaded until you explicitly load them via
+ * corresponding service.
+ *
+ */
 @Entity
 @Table(name = "offers")
 public class Offer {
@@ -32,20 +42,37 @@ public class Offer {
 
     @Getter
     @Setter
-    @ManyToOne(optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH})
+    @ManyToOne(optional = false, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "category_id")
     private Category category;
 
 
     @Getter
     @Setter
-    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH})
-    @JoinTable(
-            name = "offer_tag",
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "offer_tag",
             joinColumns = @JoinColumn(name = "offer_id"),
-            inverseJoinColumns = @JoinColumn(name = "tag_id")
-    )
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
     private Set<Tag> tags = new HashSet<>();
+
+    /**
+     * add tag to the offer.
+     * Tag are distinguished by their name.
+     *
+     * @param tag to be added
+     * @return false, if tag was already present in offer.
+     */
+    public boolean addTag(Tag tag) {
+        return tags.add(tag);
+    }
+
+    /**
+     * remove tag from offer
+     * @param tag true, of offer contained that tag.
+     */
+    public void removeTag(Tag tag) {
+        tags.remove(tag);
+    }
 
     @Override
     public String toString() {
