757abf0ed04a099d9cbfa2e3c54750d3fe130325
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-14 16:25:10 +0300

    feat: finish with Category DAO (transaction-scope entity manager)

--- catalog/src/main/java/com/netcracker/edu/varabey/dao/CategoryDaoImpl.java
+++ catalog/src/main/java/com/netcracker/edu/varabey/dao/CategoryDaoImpl.java
@@ -1,95 +1,102 @@
 package com.netcracker.edu.varabey.dao;
 
 import com.netcracker.edu.varabey.entity.Category;
-import com.netcracker.edu.varabey.utils.PostgreSQLDatabaseManager;
+import com.netcracker.edu.varabey.utils.PostgreSQLDatabaseEntityManagerFactory;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import java.util.Collection;
 
+/**
+ * Service for category. Uses transactional scope entity manager.
+ */
 public class CategoryDaoImpl implements CategoryDao {
-    private EntityManager em = PostgreSQLDatabaseManager.getInstance().getEntityManager();
+    private EntityManagerFactory emf = PostgreSQLDatabaseEntityManagerFactory.getInstance();
 
+    /**
+     * create new category in catalog. At this point category id should be null,
+     * offers should be empty, name should be unique.
+     * @param category with null id
+     * @return created category with initialized id
+     */
     @Override
     public Category create(Category category) {
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        /* ensure transaction gets closed */
-        try {
-            em.persist(category);
-        } catch (Exception e) {
-            tx.rollback();
-            throw e;
-        }
-        tx.commit();
-
-        em.detach(category);
-        return category;
-    }
-
-    @Override
-    public Collection<Category> createAll(Collection<Category> categories) {
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        try {
-            categories.forEach(category -> em.persist(category));
-        } catch (Exception e) {
-            tx.rollback();
-            throw e;
-        }
-        tx.commit();
-
-        categories.forEach( category -> em.detach(category));
-        return categories;
-    }
-
-    @Override
-    public Category read(Long id) {
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        Category category = em.find(Category.class, id);
-        if (category != null) em.refresh(category);
-        tx.commit();
-        if (category != null) em.detach(category);
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(category);
+        em.getTransaction().commit();
+        em.close();
         return category;
     }
 
     /**
-     * Merge the state of the given category (only if it is previously persisted category)
-     * into the current persistence context.
+     * same as <code>create(Category category)</code>, but for the whole
+     * Collection of categories
+     * @param categories with null ids
+     * @return Collection of created categories
+     */
+    @Override
+    public Collection<Category> createAll(Collection<Category> categories) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        categories.forEach(em::persist);
+        em.getTransaction().commit();
+        em.close();
+        return categories;
+    }
+
+    /**
+     * retrieve category from the Catalog.
+     * @param id of wanted category
+     * @return retrieved category, or null if nothing found.
+     */
+    @Override
+    public Category read(Long id) {
+        EntityManager em = emf.createEntityManager();
+        Category category = em.find(Category.class, id);
+        em.close();
+        return category;
+    }
+
+    /**
+     * Merge the state of the given category into the catalog.
      *
      * @param category  category instance
-     * @return the managed category that the state was merged to
-     * @throws IllegalArgumentException if instance is a new or removed category
+     * @return updated category.
+     * @throws IllegalArgumentException if category is new or removed category
      */
     @Override
     public Category update(Category category) {
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
 
         Query q = em.createQuery("SELECT count(c.id) FROM Category c where c.id = ?1");
         q.setParameter(1, category.getId());
         long persistedCategoryCount = (Long) q.getSingleResult();
         if (persistedCategoryCount == 0) {
-            tx.rollback();
+            em.getTransaction().rollback();
+            em.close();
             throw new IllegalArgumentException("unable to update category, that is not stored in database");
         }
 
         category = em.merge(category);
-        tx.commit();
-
-        em.detach(category);
+        em.getTransaction().commit();
+        em.close();
         return category;
     }
 
+    /**
+     * removes category and all related offers from the catalog.
+     * @param id of category to be removed
+     */
     @Override
     public void delete(Long id) {
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
         Category c = em.getReference(Category.class, id);
         em.remove(c);
-        tx.commit();
+        em.getTransaction().commit();
+        em.close();
     }
 }
