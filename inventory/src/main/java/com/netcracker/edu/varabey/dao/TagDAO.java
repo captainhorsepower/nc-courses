ecd4240ef9eeaef14547d21d97607b20a5682b1c
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-21 12:03:36 +0300

    refactor: rename dao files

--- inventory/src/main/java/com/netcracker/edu/varabey/dao/TagDAO.java
+++ inventory/src/main/java/com/netcracker/edu/varabey/dao/TagDAO.java
@@ -4,7 +4,7 @@ import com.netcracker.edu.varabey.entity.Tag;
 
 import java.util.Collection;
 
-public interface TagService {
+public interface TagDAO {
     Tag create(Tag tag);
     Collection<Tag> createAll(Collection<Tag> tag);
     Tag read(Long id);
