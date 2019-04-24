b3321255e1e1a30947c79e48b38dd94184af401f
Artem Vorobej <captainhorsepower228@gmail.com>
2019-04-24 11:27:41 +0300

    feat: add DI [customer management]

--- customer-management/src/main/java/com/netcracker/edu/varabey/entity/Customer.java
+++ customer-management/src/main/java/com/netcracker/edu/varabey/entity/Customer.java
@@ -1,36 +1,35 @@
 package com.netcracker.edu.varabey.entity;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.StringJoiner;
 
-@Entity
+@Data
 @NoArgsConstructor
+@Entity
 @Table(name = "customers")
 public class Customer {
 
-    @Getter
-    @Setter
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue
     @Column(name = "customer_id")
     private Long id;
 
     @NonNull
-    @Getter
-    @Setter
-    @Column(name = "customer_fio")
+    @Column(name = "customer_fio", nullable = false)
     private String fio;
 
     @NonNull
-    @Getter
-    @Setter
-    @Column(name = "customer_age")
-    private int age;
+    @Column(name = "customer_age", nullable = false)
+    private Integer age;
+
+    public Customer(@NonNull String fio, @NonNull Integer age) {
+        this.fio = fio;
+        this.age = age;
+    }
 
     @Override
     public String toString() {
