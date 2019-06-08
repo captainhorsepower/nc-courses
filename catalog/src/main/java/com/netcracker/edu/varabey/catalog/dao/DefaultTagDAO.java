48c9d3306f95e0b44d2f067e6c6d0226ebad6e9a
Artem Vorobej <captainhorsepower228@gmail.com>
2019-06-08 17:00:18 +0300

    docs: update docs for tagDao

--- catalog/src/main/java/com/netcracker/edu/varabey/catalog/dao/DefaultTagDAO.java
+++ catalog/src/main/java/com/netcracker/edu/varabey/catalog/dao/DefaultTagDAO.java
@@ -5,24 +5,18 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import java.util.Collection;
-import java.util.List;
 
 /**
- * Service class for Tags. Allows for creating, updating and removing tags from
- * an underlying database. Incapsulated EntityManager's scope is transactional
- * for simplicity and security. So performance might suffer just a little
- * tiny bit. If performance is a bottleneck for you, try another implementation.
- */
+ * DAO class for Tags. Allows for creating, updating and removing tags from the Catalog database.*/
 @Repository
 public class DefaultTagDAO implements TagDAO {
     @PersistenceContext
     private EntityManager em;
 
     /**
-     * creates a tags in the underlying database.
-     * Tag should have null id and offers should be empty.
+     * Creates a tags in the Catalog database.
+     * Tag should have null id and tag.offers should be empty.
      * Otherwise, expect issues with data.
      *
      * @param tag with set unique name and nothing else
@@ -35,7 +29,7 @@ public class DefaultTagDAO implements TagDAO {
     }
 
     /**
-     * Same rules as for <code>save(Tag tags)</code>.
+     * Creates all given tags in the Catalog database.
      *
      * @param tags with set names and nothing else.
      * @return given collection of tags with initialized ids
@@ -47,33 +41,33 @@ public class DefaultTagDAO implements TagDAO {
     }
 
     /**
-     * retrieves the tags from the database by id.
-     * @param id - must not be null
-     * @return tags with everything set.
+     * Retrieves the tag from the Catalog database.
+     * @param id unique tag id
+     * @return found tag.
      */
     @Override
     public Tag findById(Long id) {
         return em.find(Tag.class, id);
     }
 
+    /**
+     * Retrieves the tag from the Catalog database.
+     * @param id unique tag name
+     * @return found tag.
+     */
     @Override
     public Tag findByName(String name) {
-        TypedQuery<Tag> q = em.createQuery(
-                "SELECT t FROM Tag t where t.name = :name",
-                Tag.class
-        );
-        q.setParameter("name", name);
-
-        List<Tag> list = q.setMaxResults(1).getResultList();
-        return list.stream().findFirst().orElse(null);
+        return em.createNamedQuery("Tag.findByName", Tag.class)
+                .setParameter("name", name)
+                .setMaxResults(1).getResultList()
+                .stream()
+                .findFirst()
+                .orElse(null);
     }
 
     /**
-     * removes a tags from the database.
-     *
-     * @param id of the tags to be removed.
-     * @throws javax.persistence.EntityNotFoundException if there is no tags
-     *      with given id in the database.
+     * Removes a tag from the Catalog database.
+     * @param id of the tag to be removed.
      */
     @Override
     public void delete(Long id) {
