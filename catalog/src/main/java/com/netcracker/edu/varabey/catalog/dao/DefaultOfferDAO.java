cce41f378c46fcb3e8cacd8b7920971b71291351
Artem Vorobej <captainhorsepower228@gmail.com>
2019-06-08 17:04:29 +0300

    docs: move docs to the interfaces

--- catalog/src/main/java/com/netcracker/edu/varabey/catalog/dao/DefaultOfferDAO.java
+++ catalog/src/main/java/com/netcracker/edu/varabey/catalog/dao/DefaultOfferDAO.java
@@ -19,14 +19,6 @@ public class DefaultOfferDAO implements OfferDAO {
     @PersistenceContext
     private EntityManager em;
 
-    /**
-     * Create an offer in the database.
-     * If tags and category inside offer, have set Ids, it might introduce unexpected
-     * side effects like tag.name update, etc. Be careful.
-     *
-     * @param offer NEW (TRANSIENT) offer entity. Offer.id should be null.
-     * @return created offer.
-     */
     @Override
     public Offer save(Offer offer) {
 
@@ -37,59 +29,27 @@ public class DefaultOfferDAO implements OfferDAO {
         return em.merge(offer);
     }
 
-    /**
-     * Retrieve offer from the database.
-     * @param id unique offer id
-     * @return offer with given id
-     */
     @Override
     public Offer findById(Long id) {
         return em.find(Offer.class, id);
     }
 
-    /**
-     * Retrieve all offers from the catalog database.
-     * @return List of all offers in the catalog.
-     */
     @Override
     public List<Offer> findAll() {
         return em.createNamedQuery("Offer.findAll", Offer.class).getResultList();
     }
 
-    /**
-     * Merge the state of the given offer into the catalog database.
-     * Allows you to update:
-     *  -price (value only)
-     *  -remove tags
-     *  -add tags (you can create new tags,
-     *          but then it's id should be null when you pass it to this method)
-     *  -change category (you can create new one, -||-)
-     *
-     * You should NOT change names of existing tags nor categories.
-     *
-     * @param offer  offer instance with updated fields
-     * @return updated offer
-     */
     @Override
     public Offer update(Offer offer) {
         return em.merge(offer);
     }
 
-    /**
-     * Completely removes offer from the Catalog database.
-     * @param id unique id of target offer.
-     */
     @Override
     public void deleteById(Long id) {
         Offer offer = em.getReference(Offer.class, id);
         em.remove(offer);
     }
 
-    /**
-     * Finds all offers having given category
-     * @param category query param
-     * @return list of all offers with given category.
-     */
     @Override
     public List<Offer> findAllByCategory(Category category) {
         return em.createNamedQuery("Offer.findAllByCategory", Offer.class)
@@ -97,11 +57,6 @@ public class DefaultOfferDAO implements OfferDAO {
                 .getResultList();
     }
 
-    /**
-     * Finds all offers having all tags.
-     * @param tags query list param.
-     * @return list of offers having all given tags.
-     */
     @Override
     public List<Offer> findAllWithTags(Collection<Tag> tags) {
         return em.createNamedQuery("Offer.findAllHavingTags", Offer.class)
@@ -112,12 +67,6 @@ public class DefaultOfferDAO implements OfferDAO {
                 .getResultList();
     }
 
-    /**
-     * Finds all offers with price in range.
-     * @param lowerBound inclusive
-     * @param upperBound inclusive
-     * @return list of all offers with price within given range.
-     */
     @Override
     public List<Offer> findAllWithPriceInRange(Double lowerBound, Double upperBound) {
         return em.createNamedQuery("Offer.findAllWithPriceInRange", Offer.class)
--- catalog/src/main/java/com/netcracker/edu/varabey/catalog/dao/DefaultTagDAO.java
+++ catalog/src/main/java/com/netcracker/edu/varabey/catalog/dao/DefaultTagDAO.java
@@ -14,47 +14,23 @@ public class DefaultTagDAO implements TagDAO {
     @PersistenceContext
     private EntityManager em;
 
-    /**
-     * Creates a tags in the Catalog database.
-     * Tag should have null id and tag.offers should be empty.
-     * Otherwise, expect issues with data.
-     *
-     * @param tag with set unique name and nothing else
-     * @return given tags with initialized id.
-     */
     @Override
     public Tag save(Tag tag) {
         em.persist(tag);
         return tag;
     }
 
-    /**
-     * Creates all given tags in the Catalog database.
-     *
-     * @param tags with set names and nothing else.
-     * @return given collection of tags with initialized ids
-     */
     @Override
     public Collection<Tag> saveAll(Collection<Tag> tags) {
         tags.forEach(em::persist);
         return tags;
     }
 
-    /**
-     * Retrieves the tag from the Catalog database.
-     * @param id unique tag id
-     * @return found tag.
-     */
     @Override
     public Tag findById(Long id) {
         return em.find(Tag.class, id);
     }
 
-    /**
-     * Retrieves the tag from the Catalog database.
-     * @param id unique tag name
-     * @return found tag.
-     */
     @Override
     public Tag findByName(String name) {
         return em.createNamedQuery("Tag.findByName", Tag.class)
@@ -65,10 +41,6 @@ public class DefaultTagDAO implements TagDAO {
                 .orElse(null);
     }
 
-    /**
-     * Removes a tag from the Catalog database.
-     * @param id of the tag to be removed.
-     */
     @Override
     public void delete(Long id) {
         Tag tag = em.getReference(Tag.class, id);
