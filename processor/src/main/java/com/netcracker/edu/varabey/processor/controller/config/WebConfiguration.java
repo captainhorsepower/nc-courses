15fa648d0615bbc2819de773a94ddabca3c3af3a
Artem Vorobej <captainhorsepower228@gmail.com>
2019-06-09 13:22:58 +0300

    feat: add eureka for automated service discovery

--- processor/src/main/java/com/netcracker/edu/varabey/processor/controller/config/WebConfiguration.java
+++ processor/src/main/java/com/netcracker/edu/varabey/processor/controller/config/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.netcracker.edu.varabey.processor.controller.config;
 
 import com.netcracker.edu.varabey.processor.exception.handler.RestTemplateResponseErrorHandler;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.ResponseErrorHandler;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class WebConfiguration {
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.setErrorHandler(restTemplateResponseErrorHandler());
