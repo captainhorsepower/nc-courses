ad11b8b733304c521fd469df4da8bef270ea7818
Artem Vorobej <captainhorsepower228@gmail.com>
2019-06-09 10:18:00 +0300

    feat: add order status validation

--- inventory/src/main/java/com/netcracker/edu/varabey/inventory/web/controller/dto/transformer/OrderTransformer.java
+++ inventory/src/main/java/com/netcracker/edu/varabey/inventory/web/controller/dto/transformer/OrderTransformer.java
@@ -3,10 +3,16 @@ package com.netcracker.edu.varabey.inventory.web.controller.dto.transformer;
 import com.netcracker.edu.varabey.inventory.data.entity.Customer;
 import com.netcracker.edu.varabey.inventory.data.entity.Order;
 import com.netcracker.edu.varabey.inventory.data.entity.OrderItem;
+import com.netcracker.edu.varabey.inventory.data.entity.utils.OrderStatus;
 import com.netcracker.edu.varabey.inventory.web.controller.dto.CustomerDTO;
 import com.netcracker.edu.varabey.inventory.web.controller.dto.OrderDTO;
 import com.netcracker.edu.varabey.inventory.web.controller.dto.OrderItemDTO;
+import com.netcracker.edu.varabey.inventory.web.validation.exceptions.OrderException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class OrderTransformer implements Transformer<Order, OrderDTO> {
@@ -26,7 +32,17 @@ public class OrderTransformer implements Transformer<Order, OrderDTO> {
 
         Order order = new Order(customer, orderDTO.getCreatedOnDate());
         order.setPaid((orderDTO.isPaid() == null) ? false : orderDTO.isPaid());
-        order.setStatus(orderDTO.getOrderStatus());
+        try {
+            order.setStatus(OrderStatus.valueOf(orderDTO.getOrderStatus().toUpperCase()));
+        } catch (IllegalArgumentException e) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("\'").append(orderDTO.getOrderStatus()).append("\'")
+                    .append(" is invalid OrderStatus. Consider one of the following: ")
+                    .append(Stream.of(OrderStatus.values())
+                            .map(OrderStatus::toString)
+                            .collect(Collectors.joining(", ")));
+            throw new OrderException(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
         orderDTO.getItems().stream()
                 .map(itemTransformer::toEntity)
                 .forEach(order::addItem);
@@ -39,7 +55,7 @@ public class OrderTransformer implements Transformer<Order, OrderDTO> {
         dto.setId(order.getId());
         dto.setEmail(order.getCustomer().getEmail());
         dto.setPaid(order.isPaid());
-        dto.setOrderStatus(order.getStatus());
+        dto.setOrderStatus(order.getStatus().toString());
         dto.setCreatedOnDate(order.getCreatedOnDate());
         order.getItems().stream()
                 .map(itemTransformer::toDto)
